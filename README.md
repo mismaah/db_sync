57b402b2be9caa8dff5fe363dab2b28e894d7133
mismaah <mismaah>
2024-12-27 09:57:06 +0000

    feat: pass parameters to script

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ To add a new source, add a file to the `./sources` directory following the forma
 The `modify` variable in the source can be set to `false` to ensure that the source is read only and not to be modified.
 
 Backups are stored in `./backups`
+
+The following arguments can be passed to the script.
+- s: Source
+- sd: Source database
+- t: Target source
+- td: Target source database
+- to: Target source owner role
+
+For example the below command will restore backup from database `db1` in source `s1` into database `db2` in source `s2` with the role `postgres` as the owner.
+```bash
+bash db_sync.sh --s s1 --sd db1 --t s2 --td db2 --to postgres
+```
--- db_sync.sh
+++ db_sync.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
+# get params
+while [ $# -gt 0 ]; do
+   if [[ $1 == *"--"* ]]; then
+        v="${1/--/}"
+        declare $v="$2"
+   fi
+  shift
+done
+
 PS3="Select source: "
 sources=`ls ./sources`
 echo ""
-select source in $sources
-do
-    echo "Selected source: $source"
-    break
-done
+if [[ $s ]]; then
+    source=$s
+else
+    select source in $sources
+    do
+        echo "Selected source: $source"
+        break
+    done
+fi
 source ./sources/$source
 source_pwd=$password
 export PGPASSWORD=$source_pwd
@@ -18,11 +31,15 @@ PS3="Select source db: "
 source_dbs=$(psql --dbname=$db_string/postgres -t -c 'SELECT datname FROM pg_database;')
 
 echo ""
-select source_db in $source_dbs
-do
-    echo "Selected source db: $source_db"
-    break
-done
+if [[ $sd ]]; then
+    source_db=$sd
+else
+    select source_db in $source_dbs
+    do
+        echo "Selected source db: $source_db"
+        break
+    done
+fi
 
 date=$(date +"%Y-%m-%dT%H-%M-%S")
 file=./backups/$source/$source_db/$date.dump
@@ -30,23 +47,35 @@ mkdir -p "${file%/*}"
 export PGPASSWORD=$source_pwd
 pg_dump -Fc -v --dbname=$db_string/$source_db > $file
 date=$(date +"%Y-%m-%dT%H:%M:%S")
+echo $date
 echo "$date Backed up $source_db in $source" >> sync.log
 
 PS3="Select target: "
 
 echo ""
-select target in $sources
-do
-    echo "Selected target: $target"
-    break
-done
+if [[ $t ]]; then
+    target=$t
+else
+    select target in $sources
+    do
+        echo "Selected target: $target"
+        break
+    done
+fi
+
+if [[ $td ]]; then
+    target_db=$td
+else
+    target_db=$source_db
+fi
+
 source ./sources/$target
 target_pwd=$password
 export PGPASSWORD=$target_pwd
 target_db_string=postgresql://$user@$host:$port
 target_dbs=$(psql --dbname=$target_db_string/postgres -t -c 'SELECT datname FROM pg_database;')
-if ! [[ $target_dbs == *"$source_db"* ]]; then
-    echo "Database $source_db does not exist in $target"
+if ! [[ $target_dbs == *"$target_db"* ]]; then
+    echo "Database $target_db does not exist in $target"
     exit 100
 fi
 if [[ "$modify" == "false" ]]; then
@@ -54,18 +83,16 @@ if [[ "$modify" == "false" ]]; then
     exit 100
 fi
 
-read -p "Which role should be the owner: " owner
-
-echo "This will copy $source_db in $source into $target"
+echo "This will copy $source_db in $source into $target_db in $target"
 
 read -p "Are you sure? (y/N)" -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     export PGPASSWORD=$target_pwd
-    pg_restore -Fc -v --no-privileges --no-owner --role=$owner --clean --dbname=$target_db_string/$source_db $file
+    pg_restore -Fc -v --no-privileges --no-owner --role=$to --clean --dbname=$target_db_string/$target_db $file
     # pg_restore -Fc -v --clean --dbname=$target_db_string/$source_db $file
     date=$(date +"%Y-%m-%dT%H:%M:%S")
-    echo "$date Restored $source_db into $target" >> sync.log
+    echo "$date Restored backup from $source_db in $source into $target_db in $target" >> sync.log
 fi
 echo "Exiting"
